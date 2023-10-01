fb60853fee65a58106cbc2c8d4b29cdddd0de04f
Bee_Coder <141222317+thebeecoder@users.noreply.github.com>
2023-10-01 23:30:22 +0500

    Update app.py

--- WelcareWebapp/app.py
+++ WelcareWebapp/app.py
@@ -103,7 +103,7 @@ def fetch_user_info(cursor, user_id):
         return None
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/github/WelcareWebapp', methods=['GET', 'POST'])
 def role_select():
     return render_template('role_select.html')
 
